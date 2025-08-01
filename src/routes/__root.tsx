3a4ed89976265224bd06268a87230e16cdb3bd20
Jason Hemmy <jhemmy@mergeworld.com>
2025-08-01 16:03:31 -0500

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,15 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Header />
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
+      <Footer />
+      <TanStackRouterDevtools />
     </>
   ),
 });
