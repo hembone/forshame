501a2bd68dc130a4ef16133de89ac034a9befa6f
Jason Hemmy <jhemmy@mergeworld.com>
2025-08-01 15:24:21 -0500

    Setting up the framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './src/routes',
+      generatedRouteTree: './src/routeTree.gen.ts',
+      routeFileIgnorePrefix: '-',
+      quoteStyle: 'single',
+    }),
+    react(),
+    tailwindcss()
+  ],
 })
